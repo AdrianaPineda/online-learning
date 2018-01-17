029f04a197f747b9f70371b77a64c2aa385d848b
Adriana Pineda <am.pineda206@gmail.com>
2018-01-17 07:29:56 -0500

    Add section 4 until lecture #6

--- Exercise Files/04_05/script.js
+++ Exercise Files/04_05/script.js
@@ -1,4 +1,4 @@
-function findBiggestFraction(a,b) {
+/*function findBiggestFraction(a,b) {
     var result;
     a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
     return result;
@@ -12,3 +12,16 @@ var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 console.log("First fraction result: ", firstFraction);
 console.log("Second fraction result: ", secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+*/
+
+var firstFraction = 5/7;
+var secondFraction = 18/25;
+
+// Anonymous function
+var biggest = function(a, b) {
+  var result;
+  a>b ? result = ["a: ", a] : result = ["b: ", b];
+  return result;
+}
+
+console.log(biggest(firstFraction, secondFraction)); // Run the anonymous function
