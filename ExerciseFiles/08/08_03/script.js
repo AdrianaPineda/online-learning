0fac24945205683d5a79e9f4d3ec150291d21085
Adriana Pineda <apineda@twilio.com>
2018-03-19 16:23:50 -0500

    Add section 3 of 8

--- ExerciseFiles/08/08_03/script.js
+++ ExerciseFiles/08/08_03/script.js
@@ -12,12 +12,24 @@ const theTimer = document.querySelector(".timer");
 
 
 // Match the text entered with the provided text on the page:
-
+function spellCheck() {
+  let textEntered = testArea.value;
+  console.log(textEntered);
+}
 
 // Start the timer:
-
+function start() {
+  let textEnteredLength = testArea.value.length;
+  console.log(textEnteredLength);
+  // keypress event happens before the content is entered into the value
+}
 
 // Reset everything:
-
+function reset() {
+  console.log('Reset button has been pressed!');
+}
 
 // Event listeners for keyboard input and the reset button:
+testArea.addEventListener('keypress', start, false);
+testArea.addEventListener('keyup', spellCheck, false);
+resetButton.addEventListener('click', reset, false);
