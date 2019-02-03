360a5a0af0cc7eac5f6a59456200448793a6ea69
Adriana Pineda <apineda@twilio.com>
2019-02-03 16:49:56 -0500

    Add method to support subscribe method

--- 1-vanilla-js/src/entries/redux.js
+++ 1-vanilla-js/src/entries/redux.js
@@ -43,11 +43,21 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const $container = document.getElementById('playlist')
-const playlist = store.getState()
-playlist.forEach(item => {
-  const template = document.createElement('p')
-  template.textContent = item.title
-  $container.appendChild(template)
-});
+function render() {
+  const $container = document.getElementById('playlist')
+  const playlist = store.getState()
+  $container.innerHTML = ''
+  playlist.forEach(item => {
+    const template = document.createElement('p')
+    template.textContent = item.title
+    $container.appendChild(template)
+  });
+}
 
+render()
+
+function handleChange() {
+  render()
+}
+
+store.subscribe(handleChange)
