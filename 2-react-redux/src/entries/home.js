0794b15af0820d33c273915ed59dc6fb262dbf09
Adriana Pineda <apineda@twilio.com>
2019-02-04 06:24:46 -0500

    Create store

--- 2-react-redux/src/entries/home.js
+++ 2-react-redux/src/entries/home.js
@@ -2,6 +2,21 @@ import React from 'react'; // build components
 import { render } from 'react-dom'; // render components
 import Home from '../pages/containers/home';
 import data from '../api.json'
+import { createStore } from 'redux';
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+const initialState = {
+  data: {
+    ...data
+  }
+}
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState())
 
 const homeContainer = document.getElementById('home-container')
 render(<Home data={data}/>,homeContainer) // React knows this is a component (<Media /> HTML tag)
