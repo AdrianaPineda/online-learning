8293086072b77fca447067c7a5a6a5a4a120eb76
AdrianaPineda <am.pineda206@gmail.com>
2019-02-10 18:15:18 -0500

    Add new folder for advanced concepts of redux. Starting with normalizing data

--- 3-advanced-redux/src/entries/home.js
+++ 3-advanced-redux/src/entries/home.js
@@ -1,17 +1,21 @@
 import React from 'react'; // build components
 import { render } from 'react-dom'; // render components
 import Home from '../pages/containers/home';
-import data from '../api.json'
+// import data from '../api.json'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers/data';
+import data from '../schemas/index.js';
 
 console.log(data)
+// console.log(normalizedData)
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
 const initialState = {
   data: {
-    ...data
+    // ...data
+    entities: data.entities,
+    categories: data.result.categories
   },
   search: []
 }
@@ -24,7 +28,7 @@ const store = createStore(
   enhancer
 )
 
-console.log(store.getState())
+// console.log(store.getState())
 
 const homeContainer = document.getElementById('home-container')
 
---
+++ 3-advanced-redux/src/widgets/components/search.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './search.css'
+
+// function Search(props) {
+//   return(
+//     <form action=""></form>
+//   )
+// }
+
+// Another option:
+// const Search = () => {
+//   return(
+//     // ...
+//   )
+// }
+
+const Search = (props) => (
+  <form 
+    className="Search"
+    onSubmit={props.handleSubmit}
+  >
+    <input 
+      ref={props.setRef}
+      type="text" 
+      placeholder="Look for your favorite videos" 
+      className="Search-input"
+      // defaultValue="Luis Fonsi"
+      value={props.value}
+      onChange={props.handleChange}
+    />
+  </form>
+)
+
+export default Search;
+
+// Option to set a default value and get input content:
+// 1) ref && defaultValue. Just value won't let me edit the input
+// 2) value && onChange
+
